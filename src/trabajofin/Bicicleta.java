03a1250acf72c12188caa2d14869defd9bf2c131
imfloopa <0512025073@alumnos.unp.edu.pe>
2026-01-18 08:36:13 -0500

    Muñoz agregó comentarios

--- src/trabajofin/Bicicleta.java
+++ src/trabajofin/Bicicleta.java
@@ -5,7 +5,7 @@ public class Bicicleta extends Automoviles implements Funciones {
     
     
     
-    
+
     @Override
     public void verificarEstado(){
         System.out.println("La bicicleta se encuentra en buen estado");
