bf0f4c564000cfa9479325a20d560bdbd3ceb66c
Juansebastianflorescordova-Ops <juansebastianflorescordova@gmail.com>
2026-01-18 02:39:45 -0500

    limpiando main

--- src/trabajofin/Trabajofin.java
+++ src/trabajofin/Trabajofin.java
@@ -4,7 +4,7 @@ package trabajofin;
 public class Trabajofin {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //Rama nueva
+        
+    }
 
 }
