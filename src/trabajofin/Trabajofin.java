33e507ddf2af427c35450ae6624287a7021b6f73
Juansebastianflorescordova-Ops <juansebastianflorescordova@gmail.com>
2026-01-18 02:11:10 -0500

    mi primer hola mundo

--- src/trabajofin/Trabajofin.java
+++ src/trabajofin/Trabajofin.java
@@ -1,19 +1,10 @@
 
 package trabajofin;
 
-/**
- *
- * @author Charly Cimino
- * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
- * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
- */
 public class Trabajofin {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World!");
     }
 
 }
