9dfe4d95287a18efeab1afbb3f9a3711c79297dc
Leonardo-dev007 <Leoonardo415@gmail.com>
2026-01-18 02:36:18 -0500

    Restauracion

--- src/trabajofin/Trabajofin.java
+++ src/trabajofin/Trabajofin.java
@@ -5,14 +5,6 @@ public class Trabajofin {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // TODO code application logic here
-
-        //HOLA MUNDO
-
-
-
-
-        //RAMA 2
-    }
+        //Rama nueva
 
 }
