cdf170bbb4d90b7a34c69cbb3dfbbdba6172be51
Leonardo-dev007 <Leoonardo415@gmail.com>
2026-01-18 02:59:59 -0500

    Nuevarama

--- src/trabajofin/Trabajofin.java
+++ src/trabajofin/Trabajofin.java
@@ -1,21 +1,8 @@
 
 package trabajofin;
 
-/**
- *
- * @author Charly Cimino
- * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
- * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
- */
 public class Trabajofin {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-
-        //HOLA MUNDO
     }
 
 }
